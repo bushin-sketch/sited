a7cadf1a482609f8d29c083562b016c835d17b36
bushin-sketch <kamillyuzumaki@gmail.com>
2026-05-03 16:07:34 -0300

    Delete set_admin.py

--- set_admin.py
+++
@@ -1,7 +0,0 @@
-from app import app, db, User
-with app.app_context():
-    user = User.query.filter_by(email='SEU_EMAIL_AQUI').first()
-    if user:
-        user.is_admin = True
-        db.session.commit()
-        print("Bem vindo {name}!")
